239f0d4b5eab43b4df4351d4d36b525e8b5b6679
BrianHGrant <bhgrant8@gmail.com>
2016-07-01 08:39:39 -0700

    add second passing spec: compare two equal strings

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,11 @@
 class String
-  define_method(:word_count) do |sentence|
-    false
+  define_method(:word_count) do |word|
+    base_letters = self.split("")
+    check_letters = word.split("")
+    if base_letters.eql?(check_letters)
+      true
+    else
+      false
+    end
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,5 +5,8 @@ describe('String#word_count') do
   it("will compare two different strings and return false") do
     expect(("tofu").word_count("seitan")).to(eq(false))
   end
+  it("will return true if two strings match") do
+    expect(("tempeh").word_count("tempeh")).to(eq(true))
+  end
 
 end
