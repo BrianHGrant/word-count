bb74299c616fd8f3dba02c9d27f9aa3226f101df
BrianHGrant <bhgrant8@gmail.com>
2016-07-01 11:49:36 -0700

    add additional integration spec to test reset button

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -11,4 +11,12 @@ describe('the word count path', {:type => :feature}) do
     click_button('Count')
     expect(page).to have_content('4 times')
   end
+  it('takes in user entry for a single word and a multiple word string and returns the number of times the string includes the word') do
+    visit('/')
+    fill_in('base_word', :with => 'yellow')
+    fill_in('sentence', :with => 'The yellow school bus drove on Yellow street in the yellow sun to pick up the child Yellow.')
+    click_button('Count')
+    click_button('Try Again!')
+    expect(page).to have_content('Enter a multiple word string:')
+  end
 end
